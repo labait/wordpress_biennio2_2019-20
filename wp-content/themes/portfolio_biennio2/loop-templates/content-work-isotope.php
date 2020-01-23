a88c293a0beff190843bf985e3ab020ef63194a1
Antonio Molinari <molinari@incode.it>
2020-01-23 16:11:12 +0100

    work categories

--- wp-content/themes/portfolio_biennio2/loop-templates/content-work-isotope.php
+++ wp-content/themes/portfolio_biennio2/loop-templates/content-work-isotope.php
@@ -2,7 +2,18 @@
     <a href=" <?php echo get_post_permalink(); ?>">
         <?php the_post_thumbnail("medium-large", array("class" => "image")); ?>
         <div class="info">
-            <?php the_title(); ?>
+            <?php
+            $terms = get_terms('work_category');
+            if (!empty($terms) && !is_wp_error($terms)) {
+                echo '<ul class="categories">';
+                foreach ($terms as $term) {
+                    echo '<li>' . $term->name . '</li>';
+                }
+                echo '</ul>';
+            }
+            ?>
+            <div class="title"><?php the_title(); ?></div>
         </div>
     </a>
+
 </div>
