8e36d49484f41419ccffa70a9249789277949b59
Antonio Molinari <molinari@incode.it>
2020-01-30 16:58:38 +0100

    categories, reworks

--- wp-content/themes/portfolio_biennio2/loop-templates/content-work-isotope.php
+++ wp-content/themes/portfolio_biennio2/loop-templates/content-work-isotope.php
@@ -1,8 +1,12 @@
 <div <?php post_class("grid-item wow fadeInUp"); ?> data-wow-duration="2s" id="work-<?php the_ID(); ?>">
-    <a href=" <?php echo get_post_permalink(); ?>">
+    <a class="link" href=" <?php echo get_post_permalink(); ?>">
         <?php the_post_thumbnail("medium-large", array("class" => "image")); ?>
-        <div class="info">
-            <?php
+    </a>
+    <div class="info">
+        <?php
+        the_category();
+
+        if (false) :
             $terms = get_terms('work_category');
             if (!empty($terms) && !is_wp_error($terms)) {
                 echo '<ul class="categories">';
@@ -11,9 +15,8 @@
                 }
                 echo '</ul>';
             }
-            ?>
-            <div class="title"><?php the_title(); ?></div>
-        </div>
-    </a>
-
+        endif;
+        ?>
+        <div class="title"><?php the_title(); ?></div>
+    </div>
 </div>
--- wp-content/themes/portfolio_biennio2/page-templates/homepage-isotope.php
+++ wp-content/themes/portfolio_biennio2/page-templates/homepage-isotope.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Template Name: homepage isotope
  *
@@ -8,69 +9,58 @@
  */
 
 // Exit if accessed directly.
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
 get_header();
-$container = get_theme_mod( 'understrap_container_type' );
+$container = get_theme_mod('understrap_container_type');
 ?>
 
-<?php if ( is_front_page() ) : ?>
-  <?php get_template_part( 'global-templates/hero' ); ?>
+<?php if (is_front_page()) : ?>
+	<?php get_template_part('global-templates/hero'); ?>
 <?php endif; ?>
 
+<div id="hero" class="container-fluid px-0">
+	<div class="row no-gutters">
+		<div class="col-12">
+			<?php the_post_thumbnail('full', array('class' => 'img-fluid')); ?>
+			<h1>I'm a <span id="typed1" class="typed"></span></h1>
+		</div>
+	</div>
+</div>
 
 <div class="wrapper" id="full-width-page-wrapper">
+	<div class="<?php echo esc_attr($container); ?>" id="content">
 
-	<div class="<?php echo esc_attr( $container ); ?>" id="content">
-
-		<div class="row">
-
-			<div class="col-md-12 content-area" id="primary">
-
-				<main class="site-main" id="main" role="main">
-
-				<h1>I'm a <span id="typed1" class="typed"></span></h1>
-
-					<?php while ( have_posts() ) : the_post(); ?>
-						<?php get_template_part( 'loop-templates/content', 'page' ); ?>
-					<?php endwhile; // end of the loop. ?>
-					<?php wp_reset_postdata();?>
-
-				</main><!-- #main -->
-
-			</div><!-- #primary -->
-
-		</div><!-- .row end -->
-		
 		<section id="works">
-    <div class="container">
-        <div class="row">
-            <div class="col col-12">
-                <h2>Works</h2>
-            </div>	
-        </div>			
-    </div>
-</section>	
-
-<div class="container">
-    <div class="row">
-		<div class="col col-12">
-			<div class="grid">
-				<?php
-				$query = new WP_Query(array(                 
-					'post_type' => 'work',
-					'post_status' => 'publish',
-					'posts_per_page' => -1, 					
-				)); 					
-				while ( $query->have_posts() ) : $query->the_post(); 
-				?>
-				<?php get_template_part("loop-templates/content", "work-isotope"); ?>
-				<?php endwhile; wp_reset_postdata(); ?>
+			<div class="container">
+				<div class="row">
+					<div class="col col-12">
+						<h2>Works</h2>
+					</div>
 				</div>
+			</div>
+		</section>
+
+		<div class="container">
+			<div class="row">
+				<div class="col col-12">
+					<div class="grid">
+						<?php
+						$query = new WP_Query(array(
+							'post_type' => 'work',
+							'post_status' => 'publish',
+							'posts_per_page' => -1,
+						));
+						while ($query->have_posts()) : $query->the_post();
+						?>
+							<?php get_template_part("loop-templates/content", "work-isotope"); ?>
+						<?php endwhile;
+						wp_reset_postdata(); ?>
+					</div>
+				</div>
+			</div>
 		</div>
-    </div>
-</div>
-		
+
 	</div><!-- #content -->
 
 </div><!-- #full-width-page-wrapper -->
